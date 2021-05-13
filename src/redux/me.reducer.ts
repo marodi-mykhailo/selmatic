164b65c401616924ee081fe2fbb42bb32ede0f46
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-13 13:47:29 +0200

    create reducer for me and get me data from server

---
+++ src/redux/me.reducer.ts
@@ -0,0 +1,40 @@
+import {createSlice, createAsyncThunk, PayloadAction} from "@reduxjs/toolkit";
+import {authAPI} from "../api/app.api";
+
+type CompanyType = {
+    name: string
+    taxId: string
+}
+
+type MeReducerType = {
+    id: string
+    login: string
+    firstName: string
+    lastName: string
+    email: string
+    company: CompanyType
+}
+
+
+const initialState: MeReducerType[] = []
+
+export const getMe = createAsyncThunk(
+    "me/getMe",
+    async () => {
+        const response = await authAPI.getMe()
+        return response.data
+    }
+)
+
+const meSlice = createSlice({
+    name: "me",
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [getMe.fulfilled.type]: (state, action: PayloadAction<MeReducerType[]>) => {
+            state.push(...action.payload)
+        }
+    }
+})
+
+export default meSlice.reducer;
