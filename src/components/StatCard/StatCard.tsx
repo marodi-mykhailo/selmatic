49c3bca8af0e050dcafe4ed720dc40ce6274d722
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-29 20:55:20 +0200

    small changes

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -6,27 +6,21 @@ type StatCardPropsType = {
     title: string
     subTitle: string
     link: string
-    icon: string
-    bottomBgc: string
 }
 
-const StatCard = ({backgroundColor, title, subTitle, icon, link, bottomBgc}: StatCardPropsType) => {
+const StatCard = ({backgroundColor, title, subTitle, link}: StatCardPropsType) => {
     return (
         <div className={"statCard"}>
-            <div className={"statCard__content"}
-                 style={{backgroundColor: backgroundColor}}
+            <div className={`statCard__content ${backgroundColor}`}
             >
-                <div className={"statCard-visual"}>
-                    <i className={icon}/>
-                </div>
+                <div className={"statCard-visual"}/>
                 <div className={"statCard__details"}>
-                    <div className={"statCard__details-number"}>{title}</div>
                     <div className={"statCard__details-desc"}>
                         {subTitle}
                     </div>
+                    <div className={"statCard__details-number"}>{title}</div>
                 </div>
                 <a className={"statCard__link"}
-                   style={{backgroundColor: bottomBgc}}
                    href={link}>
                     Zobacz więcej
                     <i className="far fa-arrow-alt-circle-right"/>
