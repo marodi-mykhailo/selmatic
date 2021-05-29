73268ed5b5aed0b5341a6e42a897095437964451
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-29 20:48:46 +0200

    small changes

--- src/components/MHeader/MHeader.tsx
+++ src/components/MHeader/MHeader.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './MHeader.scss';
 import {Header} from "antd/es/layout/layout";
-import logo from '../../assets/img/logo.png';
 import {BellOutlined, DownOutlined} from "@ant-design/icons";
 import {Dropdown} from "antd";
 import HeaderBellDropdown from "./Dropdown/HeaderBellDropdown/HeaderBellDropdown";
 import HeaderNameDropdown from "./Dropdown/HeaderNameDropdown/HeaderNameDropdown";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../redux/store";
-import {appReducerType} from "../../redux/app.reducer";
+import {AppReducerType} from "../../redux/app.reducer";
 import MobileSideBar from "../MobileSideBar/MobileSideBar";
 import triggerImg from "../../assets/img/sidebar_trigger.jpeg";
 import {setIsCollapsed} from "../../redux/sidebar.reducer";
@@ -19,7 +18,7 @@ const MHeader = () => {
 
         const dispatch = useDispatch()
 
-        const {isMobile, isTablet} = useSelector<AppRootStateType, appReducerType>(state => state.app)
+        const {isMobile, isTablet} = useSelector<AppRootStateType, AppReducerType>(state => state.app)
         const isCollapsed = useSelector<AppRootStateType, boolean>(state => state.sidebar.isCollapsed)
 
         const me = useSelector<AppRootStateType, MeReducerType>(state => state.me[0])
@@ -29,8 +28,8 @@ const MHeader = () => {
         }
 
         return (
-            <Header className={"header"}>
-                <img className={"header__logo"} src={logo} alt={"logo"}/>
+            <Header className={"header box-shadow"}>
+                <span className={"header__logo"}>cybersent.net</span>
                 <div className={"header__menu"}>
 
                     {!isMobile && <>
