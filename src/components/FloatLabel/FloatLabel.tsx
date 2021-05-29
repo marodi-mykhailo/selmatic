7e1c4a5692265d028c220b3fa75d7413fd88eb63
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-29 18:54:25 +0200

    delete unnecessary attribute "type", delete getFontColor func, small style changes

--- src/components/FloatLabel/FloatLabel.tsx
+++ src/components/FloatLabel/FloatLabel.tsx
@@ -4,28 +4,14 @@ import './FloatLabel.scss';
 type FloatLabelPropsType = {
     label: string
     value?: string
-    type?: "input" | "select"
 }
 
 const FloatLabel: FunctionComponent<FloatLabelPropsType> = props => {
     const [focus, setFocus] = useState(false);
-    const {children, label, value, type = "input"} = props;
+    const {children, label, value} = props;
 
     const labelClass =
         focus || (value && value.length !== 0) ? "label label-float" : "label";
-
-    const getFontColor = (type: string) => {
-        if (focus) {
-            switch (type) {
-                case "input":
-                    return "fontColor--green"
-                case "select":
-                    return "fontColor--blue"
-                default:
-                    return ""
-            }
-        }
-    }
 
     return (
         <div
@@ -34,7 +20,7 @@ const FloatLabel: FunctionComponent<FloatLabelPropsType> = props => {
             onFocus={() => setFocus(true)}
         >
             {children}
-            <label className={`${labelClass} ${getFontColor(type)}`}>{label}</label>
+            <label className={labelClass}>{label}</label>
         </div>
     );
 };
