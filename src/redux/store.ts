53f0100c45e93bcadd48e34826aa2c27e262f12e
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-29 20:57:23 +0200

    add payments history reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import messageTemplatesTableSlice from "./messageTemplatesTable.reducer";
 import relatedAccountsTableSlice from './relatedAccountsTable.reducer';
+import paymentsHistoryTableSlice from './paymentsHistoryTable.reducer';
 import codeDatabaseTableSlice from "./codeDatabaseTable.reducer";
 import transactionTableSlice from "./transactionTable.reducer"
 import monitoringTableSlice from './monitoringTable.reducer';
-import customersTableSlice from './customersTable.reducer';
+import customersSlice from './customers.reducer';
 import sidebarSlice from "./sidebar.reducer";
 import appSlice from './app.reducer';
 import meSlice from './me.reducer';
@@ -12,10 +13,11 @@ import meSlice from './me.reducer';
 const rootReducer = combineReducers({
     messageTemplatesTable: messageTemplatesTableSlice,
     relatedAccountsTable: relatedAccountsTableSlice,
+    paymentsHistoryTable: paymentsHistoryTableSlice,
     codeDatabaseTable: codeDatabaseTableSlice,
     transitionTable: transactionTableSlice,
     monitoringTable: monitoringTableSlice,
-    customersTable: customersTableSlice,
+    customers: customersSlice,
     sidebar: sidebarSlice,
     app: appSlice,
     me: meSlice,
