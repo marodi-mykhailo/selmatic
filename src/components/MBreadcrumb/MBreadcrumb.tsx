f4694ce635ed5ab35c32defef173cd88786e737b
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-05-29 20:47:35 +0200

    change separator

--- src/components/MBreadcrumb/MBreadcrumb.tsx
+++ src/components/MBreadcrumb/MBreadcrumb.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Breadcrumb} from "antd";
-import {HomeFilled, RightOutlined} from "@ant-design/icons";
 
 const {Item} = Breadcrumb;
 
@@ -18,10 +17,9 @@ const MBreadcrumb = ({steps}: MBreadcrumbPropsType) => {
     return (
         <div className={"page-bar"}>
             <Breadcrumb className={"page-breadcrumb"}
-                        separator={<RightOutlined style={{fontSize: "8px"}}/>}>
+                        separator=">">
                 <Item className={"page-breadcrumb-item"}
                       href={"/"}>
-                    <HomeFilled className={"page-breadcrumb-icon"}/>
                     Home
                 </Item>
                 {steps?.map(item => (
